797a69c7b6b4bc3cf59405462eb0a8fe83e3eff1
Louis Yasuhiro <lyasuhir@gmail.com>
2022-05-11 11:39:28 +0900

    stuff

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,6 +6,7 @@ class RestaurantsController < ApplicationController
 
   def show
     @restaurant = Restaurant.find(params[:id])
+    @review = Review.where(restaurant_id: params[:id])
   end
 
   def new
@@ -14,17 +15,16 @@ class RestaurantsController < ApplicationController
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
-    raise
     if @restaurant.save
-      redirect_to restaurants_path
+      redirect_to restaurant_path(@restaurant)
     else
-      render :show
+      render :new
     end
   end
 
   private
 
   def restaurant_params
-     params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :restaurants, only: [:index, :show, :create, :new]
+  resources :restaurants, only: [:index, :show, :create, :new] do
+    resources :reviews, only: [:index, :show]
+  end
 end
